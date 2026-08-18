4e3c89c6b679bca4d5b9f562667b5e60310c420d
Pierre Monnin <pier.monn@gmail.com>
2026-08-18 17:29:19 +0200

    Fix get_shape_info for YAGO4.6 and upload output

--- src/get_shapes_info.py
+++ src/get_shapes_info.py
@@ -10,6 +10,8 @@ from utils.yago_utils import get_yago_endpoint
 def main():
     parser = argparse.ArgumentParser(prog="get_shapes_info", description="Get shapes and number of target nodes")
     parser.add_argument("--output", dest="output", help="Output file", required=True)
+    parser.add_argument("--yago-url", dest="yago_url", help="YAGO SPARQL endpoint URL", type=str,
+                        default=None)
     parser.add_argument("-l,--log-level", dest="log_level", help="Set the logging level", type=str,
                         default="INFO", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"])
     args = parser.parse_args()
@@ -17,16 +19,17 @@ def main():
     logger = get_logger(args.log_level)
 
     logger.info("get_shapes_info: start")
-    logger.info(f"SPARQL endpoint: {yago_utils.YAGO_ENDPOINT}")
+    logger.info(f"SPARQL endpoint: {args.yago_url if args.yago_url is not None else yago_utils.YAGO_ENDPOINT}")
     logger.info(f"Output file: {args.output}")
 
-    yago_endpoint = get_yago_endpoint()
+    yago_endpoint = get_yago_endpoint(args.yago_url)
 
     shapes = []
 
     try:
         yago_endpoint.setQuery("""
-            SELECT ?shape
+            PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            SELECT DISTINCT ?shape
             WHERE
             {
                 ?shape rdf:type <http://www.w3.org/ns/shacl#NodeShape> .
@@ -42,6 +45,8 @@ def main():
 
         for s in tqdm.tqdm(shapes):
             yago_endpoint.setQuery(f"""
+                PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#>
                 SELECT (COUNT(DISTINCT ?s) AS ?targetNodes) 
                 WHERE
                 {{
